1f776ab3996c93c804f152ec280b2b0ddc3d84c2
JacobM184 <55066056+JacobM184@users.noreply.github.com>
2020-09-14 18:45:52 +1200

    Update hello_world.c

--- software/trafficController/hello_world.c
+++ software/trafficController/hello_world.c
@@ -13,9 +13,10 @@ alt_u32 tlc_timer_isr(void* context){
 // this function sets the mode value based on inputs from the SWITCHES
 int lcd_set_mode(unsigned int previousMode, FILE *lcd){
 
-
+	// read switch value
 	int output = IORD_ALTERA_AVALON_PIO_DATA(SWITCHES_BASE);
 
+	// update lcd display if necessary
 	if(lcd != NULL){
 		if(output != previousMode){
 			#define ESC 27
@@ -24,10 +25,15 @@ int lcd_set_mode(unsigned int previousMode, FILE *lcd){
 			fprintf(lcd, "CURRENT MODE: %d\n", output);
 		}
 	}
+	
+	// return mode
 	return output;
 }
 
-
+// this function controllers the behavior of the LEDs
+int simple_tlc(){
+	
+}
 
 int main()
 {
@@ -35,20 +41,38 @@ int main()
   unsigned int previousMode = 0;
 
   alt_alarm timer;
-  int lightCol = 0;
+  int lightTime = 0;
 
 
-  void* timerContext = (void*) &lightCol;
+  void* timerContext = (void*) &lightTime;
 
+  // open lcd file in write mode
   lcd = fopen(LCD_NAME, "w");
 
   while(1){
 
+	  // update the mode value
 	  previousMode = lcd_set_mode(previousMode, lcd);
+
+	  // switch statement to choose the traffic controller based on mode
+	  switch(previousMode){
+	  case 1 :
+		  simple_tlc();
+		  break;
+	  case 2 :
+		  break;
+	  case 3 :
+		  break;
+	  case 4 :
+		  break;
+	  default :
+		  break;
+	  }
 
 
   }
 
+  // close the lcd file (should not occur)
   fclose(lcd);
 
   return 0;
