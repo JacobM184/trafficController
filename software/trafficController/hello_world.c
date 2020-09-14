47820f15aee1fd7ea8a5dfe6149a4beaf79e03eb
JacobM184 <55066056+JacobM184@users.noreply.github.com>
2020-09-14 21:48:17 +1200

    Update hello_world.c

--- software/trafficController/hello_world.c
+++ software/trafficController/hello_world.c
@@ -16,6 +16,7 @@ unsigned int state = 5;
 alt_u32 tlc_timer_isr(void* context){
 
 	int *timeOut = (int*)context;
+
 	//Go to next state
 	if (state < 5){
 		state++;
@@ -57,27 +58,44 @@ void simple_tlc(void* timerContext){
 	switch (state)
 	{
 	case 0:
+		// set timer value for next
 		*timerValue = 2000;
+		// set current LEDs
 		IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, NS_GREEN);
 		break;
+
 	case 1:
+		// set timer value for next
 		*timerValue = 500;
+		// set current LEDs
 		IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, NS_YELLOW);
 		break;
+
 	case 2:
+		// set timer value for next
 		*timerValue = 6000;
+		// set current LEDs
 		IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, NS_RED);
 		break;
+
 	case 3:
+		// set timer value for next
 		*timerValue = 2000;
+		// set current LEDs
 		IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, EW_GREEN);
 		break;
+
 	case 4:
+		// set timer value for next
 		*timerValue = 500;
+		// set current LEDs
 		IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, EW_YELLOW);
 		break;
+
 	case 5:
+		// set timer value for next
 		*timerValue = 6000;
+		// set current LEDs
 		IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, EW_RED);
 		break;
 	default:
@@ -88,26 +106,36 @@ void simple_tlc(void* timerContext){
 
 int main()
 {
+	// set up LCD file and open in writing mode
 	FILE *lcd;
 	lcd = fopen(LCD_NAME, "w");
+
+	// initialize variables and flag
 	unsigned int currentMode = 0;
 	unsigned int prevMode = 0;
 	unsigned int initFlag = 1;
 
 	int lightCol = 500;
 
+	// set up timer
 	alt_alarm timer;
 	void* timerContext = (void*) &lightCol;
+
   while(1){
 
 	  // update the mode value
 	  currentMode = lcd_set_mode(currentMode, lcd);
 
+	  // check for change in mode
 	  if (currentMode != prevMode){
+
+		  // start timer on first startup
 		  if (initFlag == 1){
 			  alt_alarm_start(&timer, lightCol, tlc_timer_isr, timerContext);
 			  initFlag = 0;
 		  }
+
+		  // restart timer when mode changed and not restarted
 		  else{
 			  alt_alarm_stop(&timer);
 			  usleep(1000);
@@ -118,20 +146,27 @@ int main()
 			  alt_alarm_start(&timer, lightCol, tlc_timer_isr, timerContext);
 		  }
 
+		  // update previous mode value
 		  prevMode = currentMode;
 	  }
 
 	  // switch statement to choose the traffic controller based on mode
 	  switch(prevMode){
+
 	  case 1 :
+		  // run controller for Mode 1
 		  simple_tlc(timerContext);
 		  break;
+
 	  case 2 :
 		  break;
+
 	  case 3 :
 		  break;
+
 	  case 4 :
 		  break;
+
 	  default :
 		  break;
 	  }
@@ -139,7 +174,7 @@ int main()
 
   }
 
-  // close the lcd file (should not occur)
+  // close the LCD file (should not occur)
   fclose(lcd);
 
   return 0;
